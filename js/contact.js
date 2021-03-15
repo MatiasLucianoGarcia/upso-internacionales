f1f80707c57ce4b8e006e54db949fe5ad431f075
MatiasLucianoGarcia <mati.luciano.garica@gmail.com>
2021-03-15 11:39:26 -0300

    actualizado

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,29 @@
 function sendEmail(){
+  const name = document.querySelector('#name');
+  const surname = document.querySelector('#surname');
+  const email = document.querySelector('#email');
+  const phone = document.querySelector('#phone');
+  const message = document.querySelector('#message');
+  const body= `Nombre: ${name.value} 
+  Apellido: ${surname.value} 
+  Telefono: ${phone.value}
+  Email: ${email.value}
+  Mensaje: ${message.value}`;
+  name.value='';
+  surname.value='';
+  email.value='';
+  phone.value='';
+  message.value='';
+  console.log(name.value,surname.value,email.value,phone.value,message.value);
   Email.send({
     Host : "smtp.gmail.com",
-    Username : "mati.luciano.garcia@gmail.com",
-    Password : "pukqnygyxwwcejmg",
-    To : 'mati.luciano.garcia@gmail.com',
-    From : "mati.luciano.garcia@gmail.com",
-    Subject : "This is the subject",
-    Body : "And this is the body"
-}).then(
-  message => alert(message)
-);
+    Username : "matiascelt@gmail.com",
+    Password : "tuhhunlidtewykni",
+    To :'internacionales@upso.edu.ar',
+    From : "matiascelt@gmail.com",
+    Subject : "EMAIL WEB INTERNACIONALES",
+    Body : body
+  }).then(
+    message => alert(message)
+  );
 }
